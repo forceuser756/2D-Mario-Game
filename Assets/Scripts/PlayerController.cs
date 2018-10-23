8071e70ca8d93df81be631cf420fc800d1cf3151
TFossum1 <TFossum1@gmail.com>
2018-10-23 19:54:22 -0400

    File upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private bool facingRight = true;
+    private int count;
+
+    public float speed;
+    public float jumpforce;
+    public Text countText;
+    private AudioSource source;
+    public AudioClip jumpClip;
+    public AudioClip pickupClip;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+
+
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+	}
+
+
+    void Awake()
+    {
+
+       source = GetComponent<AudioSource>();
+
+    }
+
+    private void Update(){
+       
+    }
+
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        Debug.Log(isOnGround);
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+
+
+        
+        if (facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveHorizontal < 0)
+        {
+            Flip();
+        }
+
+	}
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground" && isOnGround)
+        {
+
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+                rb2d.velocity = Vector2.up * jumpforce;
+
+
+                //float vol = Random.Range(volLowRange, volHighRange);
+                source.PlayOneShot(jumpClip);
+
+
+
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+            source.PlayOneShot(pickupClip);
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+           void SetCountText()
+            {
+                countText.text = "Count: " + count.ToString();
+                if (count >= 8);
+            }
+        }
